597ca23e1c0a99b9263f58a2b68af0012888f15e
Elliot <e.o.johnson-hall@sms.ed.ac.uk>
2020-12-21 22:39:26 +0000

    Deleted unnecessary files

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -1,42 +1,42 @@
-import React, { useState } from "react"
-import getStripe from "../utils/stripejs"
+import React, { useState } from 'react';
+import getStripe from '../utils/stripejs';
 
 const buttonStyles = {
-  fontSize: "13px",
-  textAlign: "center",
-  color: "#000",
-  padding: "12px 60px",
-  boxShadow: "2px 5px 10px rgba(0,0,0,.1)",
-  backgroundColor: "rgb(255, 178, 56)",
-  borderRadius: "6px",
-  letterSpacing: "1.5px",
-}
+  fontSize: '13px',
+  textAlign: 'center',
+  color: '#000',
+  padding: '12px 60px',
+  boxShadow: '2px 5px 10px rgba(0,0,0,.1)',
+  backgroundColor: 'rgb(255, 178, 56)',
+  borderRadius: '6px',
+  letterSpacing: '1.5px',
+};
 
 const buttonDisabledStyles = {
-  opacity: "0.5",
-  cursor: "not-allowed",
-}
+  opacity: '0.5',
+  cursor: 'not-allowed',
+};
 
 const Checkout = () => {
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
-  const redirectToCheckout = async event => {
-    event.preventDefault()
-    setLoading(true)
+  const redirectToCheckout = async (event) => {
+    event.preventDefault();
+    setLoading(true);
 
-    const stripe = await getStripe()
+    const stripe = await getStripe();
     const { error } = await stripe.redirectToCheckout({
-      mode: "payment",
-      lineItems: [{ price: "price_1I0PdEEe4dnHfM55dVMzmOQe", quantity: 1 }],
-      successUrl: `http://localhost:8000/`,
-      cancelUrl: `http://localhost:8000/404`,
-    })
+      mode: 'payment',
+      lineItems: [{ price: 'price_1I0PdEEe4dnHfM55dVMzmOQe', quantity: 1 }],
+      successUrl: 'http://localhost:8000/',
+      cancelUrl: 'http://localhost:8000/404',
+    });
 
     if (error) {
-      console.warn("Error:", error)
-      setLoading(false)
+      console.warn('Error:', error);
+      setLoading(false);
     }
-  }
+  };
 
   return (
     <button
@@ -48,7 +48,7 @@ const Checkout = () => {
     >
       BUY MY BOOK
     </button>
-  )
-}
+  );
+};
 
-export default Checkout
+export default Checkout;
