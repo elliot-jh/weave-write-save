b1505b0413d2521d98a74e2620bddebc34758197
Elliot <e.o.johnson-hall@sms.ed.ac.uk>
2020-12-22 14:57:16 +0000

    Initial commit

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -7,8 +7,7 @@ import { Container } from '@components/global';
 const NotFoundPage = () => (
   <Layout>
     <Container>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>404</h1>
     </Container>
   </Layout>
 );
