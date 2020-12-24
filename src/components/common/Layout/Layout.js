8227b51ef8e6e340f9f4ca18931a0fe35ade901c
Elliot <e.o.johnson-hall@sms.ed.ac.uk>
2020-12-24 12:28:11 +0000

    eslint changes to layout.js

--- src/components/common/Layout/Layout.js
+++ src/components/common/Layout/Layout.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable import/no-unresolved */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
