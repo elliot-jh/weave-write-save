5beae907bef7bcd01d4b64397017215cd12cfed9
Elliot <e.o.johnson-hall@sms.ed.ac.uk>
2020-12-24 15:03:21 +0000

    yeah

--- src/pages/backend.jsx
+++ src/pages/backend.jsx
@@ -1,5 +1,23 @@
+/* eslint-disable import/no-unresolved */
+import React from 'react';
+
+import Layout from '@common/Layout';
+import Navbar from '@common/Navbar';
+import SEO from '@common/SEO';
+
 import CMS from 'netlify-cms-app';
+
 // Initialize the CMS object
 CMS.init();
 // Now the registry is available via the CMS object.
 /* CMS.registerPreviewTemplate('my-template', MyTemplate); */
+
+const Page = () => (
+  <Layout>
+    <SEO />
+    <Navbar />
+    <h1>This is a blank page.</h1>
+  </Layout>
+);
+
+export default Page;
